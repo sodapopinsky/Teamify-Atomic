1c309ac33ebbd10abb1080b8742ed426b6c9a744
sodapopinsky <Nick@Nicholass-iMac.local>
2015-12-20 12:13:47 -0600

    f

--- server.js
+++ server.js
@@ -24,64 +24,18 @@
     app.set('port', (process.env.PORT || 8080));
     console.log("App listening on port 8080");
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+    app.listen(app.get('port'), function() {
+        console.log('Node app is running on port', app.get('port'));
+    });
      // define model =================
-     var Todo = {test: "tesst"}; //
+
      
    
 
 
-    // routes ======================================================================
-
-    // api ---------------------------------------------------------------------
-    // get all todos
-    app.get('/api/todos', function(req, res) {
-     res.json({test: "test"}); 
-        // use mongoose to get all todos in the database
-       
-    });
-
-    // create todo and send back all todos after creation
-    app.post('/api/todos', function(req, res) {
-
-        // create a todo, information comes from AJAX request from Angular
-        Todo.create({
-            text : req.body.text,
-            done : false
-        }, function(err, todo) {
-            if (err)
-                res.send(err);
-
-            // get and return all the todos after you create another
-            Todo.find(function(err, todos) {
-                if (err)
-                    res.send(err)
-                res.json(todos);
-            });
-        });
-
-    });
-
-    // delete a todo
-    app.delete('/api/todos/:todo_id', function(req, res) {
-        Todo.remove({
-            _id : req.params.todo_id
-        }, function(err, todo) {
-            if (err)
-                res.send(err);
-
-            // get and return all the todos after you create another
-            Todo.find(function(err, todos) {
-                if (err)
-                    res.send(err)
-                res.json(todos);
-            });
-        });
-    });
 
      app.get('*', function(req, res) {
+
         res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
     });
 
