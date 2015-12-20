88fe5108ab1239cd3b7d48bf9ab4841f97f3f3dd
sodapopinsky <Nick@Nicholass-iMac.local>
2015-12-20 09:01:50 -0600

    f

--- server.js
+++ server.js
@@ -20,11 +20,13 @@
     app.use(methodOverride());
 
     // listen (start app with node server.js) ======================================
-    app.listen(8080);
+   
+    app.set('port', (process.env.PORT || 8080));
     console.log("App listening on port 8080");
 
-
-
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
      // define model =================
      var Todo = {test: "tesst"}; //
      
