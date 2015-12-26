86aeb07e4c6830939e76783c86b3deb7c92e5f70
Nick Spitale <nspita1@gmail.com>
2015-12-25 18:36:56 -0600

    g

--- app/js/test.js
+++
@@ -1,7 +0,0 @@
-
-
-
-module.exports = function(vars) {
-    var t = angular.module('teamify',['ui.router','satellizer']);
-}
-
---
+++ client/dist/teamify.js
@@ -0,0 +1,13 @@
+/*! teamify - v0.0.1 - 2015-12-25
+ * Copyright (c) 2015 Nick Spitale;
+ * Licensed 
+ */
+angular.module('app', [
+    'ui.router']);
+
+
+angular.module('templates.app', []);
+
+
+angular.module('templates.common', []);
+
