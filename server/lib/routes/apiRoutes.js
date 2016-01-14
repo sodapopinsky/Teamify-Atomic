3cb1a05e664d28e66bc69674e129015dcaa3efc1
Nick Spitale <nspita1@gmail.com>
2016-01-14 11:08:50 -0600

    f

--- server/lib/routes/apiRoutes.js
+++ server/lib/routes/apiRoutes.js
@@ -1,18 +1,3 @@
-var User = require('../models/user');
-var Inventory = require('../models/inventory');
-var OrderForm = require('../models/orderform');
-var Organization = require('../models/organization');
-var Projection = require('../models/projection');
-var Position = require('../models/position');
-var Task = require('../models/task');
-var Notification = require('../models/notification');
-var TaskCompletion = require('../models/taskCompletion');
-var EmployeeFile = require('../models/employeeFile');
-var Timecard = require('../models/timecard');
-var morgan = require('morgan');
-var moment = require('moment');
-var async = require('async');
-//var _ = require('lodash');
 
 exports.addRoutes = function (apiRoutes) {
 
