0cd6d15ccb99e5b899b9a51222fd3f8049943d60
Nick Spitale <nspita1@gmail.com>
2016-01-19 11:17:04 -0600

    f

--- server/lib/models/notification.js
+++ server/lib/models/notification.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 module.exports = mongoose.model('Notification', new Schema({
-    updated_at: Date,
+    updated_at: {type:Date, default:Date.now()},
     _created_by: { type: Schema.Types.ObjectId, ref: 'User' },
     requires_acknowledge: {type:Boolean, default:false},
     recipients: [{ type: Schema.Types.ObjectId, ref: 'User' }],
